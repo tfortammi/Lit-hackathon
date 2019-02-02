3707f3c680030e400b2a3b84d01ecd394c3bc9bc
tfortammi <43410270+tfortammi@users.noreply.github.com>
2019-02-03 01:02:49 +0800

    cleaned up files

--- Try 1/Form3/form_action.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $pass = $_POST['pass'];
-    $cpass = $_POST['cpass'];
-    $casetype = $_POST['CaseType[]'];
-    $others = $_POST['others'];
-?>
