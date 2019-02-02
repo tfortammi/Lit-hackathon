7439f7621d1b3406d639ead214d1eb35abec69c5
tfortammi <43410270+tfortammi@users.noreply.github.com>
2019-02-02 21:18:26 +0800

    added files

---
+++ Try 1/Form3/form_action.php
@@ -0,0 +1,8 @@
+<?php 
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $cpass = $_POST['cpass'];
+    $casetype = $_POST['CaseType[]'];
+    $others = $_POST['others'];
+?>
